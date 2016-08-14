e11e4cb4d7f182a4edcbedefdcf374f1b739ee5f
Zulfa Juniadi bin Zulkifli <zulfajuniadi@gmail.com>
2016-08-14 18:12:54 +0800

    Update login screen

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,11 +12,26 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+
+    <style type="text/css">
+        .login-logo {
+            margin-top: 10px;
+            height: 90px;
+            margin-right: 10px;
+        }
+    </style>
 </head>
 <body>
+    <br>
     <div class="container">
-        <h1>Sands Developer Portal</h1>
-        <p>It's bare because we like it that way</p>
+        <div class="clearfix">
+            <img src="/assets/images/logo.png" class="pull-left login-logo">
+            <div>
+                <h1>Sands Developer Portal</h1>
+                <p>It's bare because we like it that way</p>
+            </div>
+        </div>
+        <hr>
         @if(Session::has('error'))
             <div class="alert alert-danger" role="alert">
                 {{Session::get('error')['message']}}
